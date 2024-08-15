da4468de3d9c19acc04f66326d97555352422e7b
User <sofyamilich@gmail.com>
2024-08-15 15:38:15 +0300

    2. Пагинация на джанго. Внизу страницы + выпадающий вариант

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,14 +2,26 @@ from django.shortcuts import render
 from .models import Post
 from django.core.paginator import Paginator
 
-
 def index(request):
-
+    per_page = request.GET.get('per_page', 2)
     post = Post.objects.all().order_by('-created_at')
-    paginator = Paginator(post, 2)
+    paginator = Paginator(post, per_page)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-
-
-    # возвращает функцию render, импортированную по умолчанию в django
     return render(request, 'index.html', {'page_obj': page_obj})
+
+
+
+
+
+
+# def index(request):
+#
+#     post = Post.objects.all().order_by('-created_at')
+#     paginator = Paginator(post, 2)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#
+#
+#     # возвращает функцию render, импортированную по умолчанию в django
+#     return render(request, 'index.html', {'page_obj': page_obj})
